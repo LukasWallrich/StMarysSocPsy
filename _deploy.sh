dd2698be7c428c1dee29b3bb03421bde8be19cb0
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-09-11 11:26:51 +0100

    Prep for travis

--- _deploy.sh
+++ _deploy.sh
@@ -1,6 +1,6 @@
 # configure your name and email if you have not done so
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.email "lukas.wallrich@gmail.com"
+git config --global user.name "Lukas Wallrich"
 
 # clone the repository to the book-output directory
 git clone -b gh-pages \
