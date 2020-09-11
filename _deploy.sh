78249d03fadbaeaf668a7812c8dd7a11a3eae68b
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-09-11 12:13:23 +0100

    Travis access fixed?

--- _deploy.sh
+++ _deploy.sh
@@ -1,5 +1,5 @@
 # configure your name and email if you have not done so
-git config --global user.email "lukas.wallrich@gmail.com"
+git config --global user.email "l.wallrich@gold.ac.uk"
 git config --global user.name "Lukas Wallrich"
 
 # clone the repository to the book-output directory
