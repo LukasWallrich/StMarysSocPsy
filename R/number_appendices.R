01b004bad179705097895a45e14d3361aff4e3f2
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-09-24 15:29:14 +0100

    Fix seminar section numbering

--- R/number_appendices.R
+++ R/number_appendices.R
@@ -8,15 +8,13 @@ start_letters <- 11
 
 files <- list.files("./_book", pattern = "\\.html$")
 
-purrr::walk(file.path("_book", files), function(file_name) {
-browser()
-x <- xml2::read_html(file_name)
+purrr::walk(files, function(file_name) {
+x <- xml2::read_html(file.path("_book", file_name))
 y <- xml_find_all(x, '//*[@class="chapter"]')
 z <- y[xml_find_lgl(y,'not(number(substring(@data-level, 1, 1)+1))')]
 z <- z[-1]
 cntr <- 1
 purrr::map(z, function(x) {
-  
   chapter <- x %>% xml_attr("data-level") 
   if(stringr::str_sub(chapter, 1, 1) != LETTERS[cntr]) cntr <<- cntr + 1
   if (cntr < start_letters) {
@@ -24,5 +22,30 @@ purrr::map(z, function(x) {
   } else {
   xml_text(x) <- chapter %>% stringr::str_replace(LETTERS[cntr], LETTERS[cntr-start_letters+1])
 }})
-write_html(x, file_name)
+
+active_chapter <- xml_find_all(x, paste0('//*[@data-path="', file_name, '"]')) %>% 
+  .[1] %>% xml_attr("data-level")
+
+if (active_chapter %in% LETTERS) {
+  if (which(active_chapter==LETTERS)<start_letters) {
+    y <- xml_find_all(x, '//*[@class="header-section-number"]') %>% .[-1]
+    purrr::walk(y, function(x) {
+      xml_text(x) <- xml_text(x) %>% stringr::str_replace(active_chapter, as.character(which(active_chapter==LETTERS)))
+    })
+  } else {
+    y <- xml_find_all(x, '//*[@class="header-section-number"]')
+    xml_text(y[1]) <- xml_text(y[1]) %>% 
+      stringr::str_replace("Seminar ", "") %>% 
+      stringr::str_replace(as.character(which(active_chapter==LETTERS)), LETTERS[which(active_chapter==LETTERS)-start_letters+1])
+    purrr::walk(y[-1], function(x) {
+      xml_text(x) <- xml_text(x) %>% stringr::str_replace(active_chapter, LETTERS[which(active_chapter==LETTERS)-start_letters+1])
+    })
+    }
+  
+}
+
+y <- xml_find_all(x, '//*[@class="active"]')
+z <- y %>% xml_attr("data-level")
+
+write_html(x, file.path("_book", file_name))
 })
